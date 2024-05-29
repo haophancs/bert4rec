b6e6eb68457f957fcaa2d51d4f78ce64a5fd7b5b
Hao Phu Phan <phuhao.p1004@gmail.com>
2024-05-29 12:36:34 +0700

    Update and rename RUNME.sh to run_deploy.sh

--- run_deploy.sh
+++ run_deploy.sh
@@ -4,10 +4,9 @@ source venv/bin/activate
 mkdir -p resources/datasets resources/checkpoints resources/checkpoints resources/db logs
 pip3 install -r requirements.txt
 python3 run_dump_data.py
-gdown https://drive.google.com/u/0/uc?id=1gf4_zpHd4H-ZH625TvcSEEs95idxYGOa
-mv bert4rec_ml-25m_best.ckpt resources/checkpoints/
+wget -O resources/checkpoints/bert4rec_ml-25m_best https://uithcm-my.sharepoint.com/:u:/g/personal/18520216_ms_uit_edu_vn/EVCvHZg7QFZGlis704IiPdIBMJxIK37tcVGUM9zY-LzlCw?e=tCgA0J&download=1
 
-echo "Open logs/celery.out to check if predictor is ready!!"
+echo "Open logs/celery.out to check if the predictor is ready!!"
 echo "Open logs/uvicorn.out to get the running API url!!"
 nohup python3 -u run_redis.py > logs/redis.out &
 nohup celery -A run_api.celery worker --loglevel=info --pool=solo > logs/celery.out &
