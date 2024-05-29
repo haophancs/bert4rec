7a938df5437e511507d1d72f9f626ef7ad0880b1
Hao Phan <phuhao.p1004@gmail.com>
2024-05-29 13:44:16 +0700

    Removed .env

--- run_deploy.sh
+++ run_deploy.sh
@@ -3,6 +3,7 @@ python3 -m venv venv
 source venv/bin/activate
 mkdir -p resources/datasets resources/checkpoints resources/checkpoints resources/db logs
 pip3 install -r requirements.txt
+cp .env_org .env
 python3 run_dump_data.py
 wget -O resources/checkpoints/bert4rec_ml-25m_best https://uithcm-my.sharepoint.com/:u:/g/personal/18520216_ms_uit_edu_vn/EVCvHZg7QFZGlis704IiPdIBMJxIK37tcVGUM9zY-LzlCw?e=tCgA0J&download=1
 
